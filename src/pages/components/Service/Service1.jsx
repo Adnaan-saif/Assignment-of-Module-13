991d697e43d89f70d58ba6f1b0b45f9e16c9ec0b
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-11-08 23:35:22 +0600

    Implemented "useState" hook at service page

--- src/pages/components/Service/Service1.jsx
+++ src/pages/components/Service/Service1.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 
 const Service1 = () => {
+    const [count, setCount] = useState(0);
+    const incrementCount = () => {
+        setCount(count + 1);
+      };    
     return (
         <div>
-            
+            <h1>Service Page</h1>
+            <p>Count: {count}</p>
+            <button onClick={incrementCount}>Increment Count</button>
         </div>
     );
 };
