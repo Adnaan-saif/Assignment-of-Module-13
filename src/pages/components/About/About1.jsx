24d28af9e9dca1a61e2a84dc4d8f45e01c0970e2
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-11-08 23:29:33 +0600

    Used "useRef" hook

--- src/pages/components/About/About1.jsx
+++ src/pages/components/About/About1.jsx
@@ -1,8 +1,14 @@
+import { useRef } from 'react';
 
 const About1 = () => {
+    const innerHTMLRef = useRef(null);
+    const changeInnerHTML = () => {
+        innerHTMLRef.current.innerHTML = 'This is about using react hooks and practice building demo react project';
+      };
     return (
         <div>
-            
+            <div ref={innerHTMLRef}>This is about page</div>
+            <button onClick={changeInnerHTML}>Click to view</button>
         </div>
     );
 };
